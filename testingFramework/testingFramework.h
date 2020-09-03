02a45fb5fedfcb758c737aa7268c9af242b180d7
sandi <sandy.spyware@gmail.com>
2020-09-03 12:47:40 +0700

    first commit

---
+++ testingFramework/testingFramework.h
@@ -0,0 +1,19 @@
+//
+//  testingFramework.h
+//  testingFramework
+//
+//  Created by SANDI on 03/09/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testingFramework.
+FOUNDATION_EXPORT double testingFrameworkVersionNumber;
+
+//! Project version string for testingFramework.
+FOUNDATION_EXPORT const unsigned char testingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingFramework/PublicHeader.h>
+
+
